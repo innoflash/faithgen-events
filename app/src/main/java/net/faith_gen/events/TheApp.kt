5f9243cd8c700426ec4ae3ee036783080c4ea44f
innoflash <innocentmazando@gmail.com>
2020-01-28 18:31:57 +0200

    fixed by Kotlin

--- app/src/main/java/net/faith_gen/events/TheApp.kt
+++ app/src/main/java/net/faith_gen/events/TheApp.kt
@@ -1,6 +1,7 @@
 package net.faith_gen.events
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 import net.faithgen.sdk.SDK
 import nouri.`in`.goodprefslib.GoodPrefs
 import java.io.IOException
@@ -10,6 +11,7 @@ class TheApp: Application() {
     override fun onCreate() {
         super.onCreate()
         GoodPrefs.init(this)
+        AndroidThreeTen.init(this)
         try {
             SDK.initializeSDK(this, this.assets.open("config.json"), resources.getString(R.color.colorPrimary), null)
             SDK.initializeApiBase("http://192.168.0.112:8001/api/")
--- events/src/main/java/net/faithgen/events/EventsActivity.kt
+++ events/src/main/java/net/faithgen/events/EventsActivity.kt
@@ -2,9 +2,11 @@ package net.faithgen.events
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import net.faithgen.sdk.FaithGenActivity
 
 class EventsActivity : FaithGenActivity() {
+    val daysOfWeek = daysOfWeekFromLocale()
     override fun getPageTitle(): String {
         return Constants.EVENTS
     }
@@ -16,5 +18,6 @@ class EventsActivity : FaithGenActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events)
+        toolbar.visibility = View.GONE
     }
 }
