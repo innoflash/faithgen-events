f3d5e3407e0828ea4da5445cdf5a78782ce4084d
innoflash <innocentmazando@gmail.com>
2020-01-28 12:11:07 +0200

    sorted view holder

--- events/src/main/java/net/faithgen/events/EventsAdapter.kt
+++ events/src/main/java/net/faithgen/events/EventsAdapter.kt
@@ -2,19 +2,21 @@ package net.faithgen.events
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import net.faithgen.events.models.Event
 import net.faithgen.events.view_holders.EventViewHolder
+import net.innoflash.iosview.lists.ListItemView3
 
-class EventsAdapter(val context : Context, val events : List<Event>): RecyclerView.Adapter<EventViewHolder>() {
+class EventsAdapter(private val context : Context, private val events : List<Event>): RecyclerView.Adapter<EventViewHolder>() {
     private var layoutInflater: LayoutInflater?
 
     init {
-        layoutInflater = LayoutInflater.from(context)
+        layoutInflater = LayoutInflater.from(this.context)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
-        return EventViewHolder(layoutInflater!!.inflate(R.layout.list_item_event, parent, false))
+        return EventViewHolder(layoutInflater!!.inflate(R.layout.list_item_event, parent, false) as ListItemView3)
     }
 
     override fun getItemCount(): Int {
@@ -22,6 +24,6 @@ class EventsAdapter(val context : Context, val events : List<Event>): RecyclerVi
     }
 
     override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
-        holder.getEventsView()!!.itemHeading = "we good"
+        holder.eventView!!.itemHeading = "we good"
     }
 }
--- events/src/main/java/net/faithgen/events/view_holders/EventViewHolder.kt
+++ events/src/main/java/net/faithgen/events/view_holders/EventViewHolder.kt
@@ -1,16 +1,6 @@
 package net.faithgen.events.view_holders
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import net.innoflash.iosview.lists.ListItemView3
 
-class EventViewHolder(var itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private var eventView: ListItemView3?
-
-    fun getEventsView(): ListItemView3? {
-        return eventView
-    }
-    init {
-        eventView = itemView as ListItemView3?
-    }
-}
+class EventViewHolder(val eventView: ListItemView3) : RecyclerView.ViewHolder(eventView) {}
