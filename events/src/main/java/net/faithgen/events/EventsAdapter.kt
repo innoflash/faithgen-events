d68058421cfb3b4b3d42a3ee0224b261c6179a9d
innoflash <innocentmazando@gmail.com>
2020-01-28 12:15:37 +0200

    finished adapter

--- events/src/main/java/net/faithgen/events/EventsAdapter.kt
+++ events/src/main/java/net/faithgen/events/EventsAdapter.kt
@@ -7,16 +7,25 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import net.faithgen.events.models.Event
 import net.faithgen.events.view_holders.EventViewHolder
+import net.faithgen.sdk.SDK
 import net.innoflash.iosview.lists.ListItemView3
 
-class EventsAdapter(private val context : Context, private val events : List<Event>): RecyclerView.Adapter<EventViewHolder>() {
+class EventsAdapter(private val context: Context, private val events: List<Event>) :
+    RecyclerView.Adapter<EventViewHolder>() {
     private var layoutInflater: LayoutInflater?
 
     init {
         layoutInflater = LayoutInflater.from(this.context)
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
-        return EventViewHolder(layoutInflater!!.inflate(R.layout.list_item_event, parent, false) as ListItemView3)
+        return EventViewHolder(
+            layoutInflater!!.inflate(
+                R.layout.list_item_event,
+                parent,
+                false
+            ) as ListItemView3
+        )
     }
 
     override fun getItemCount(): Int {
@@ -24,6 +33,13 @@ class EventsAdapter(private val context : Context, private val events : List<Eve
     }
 
     override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
-        holder.eventView!!.itemHeading = "we good"
+        holder.eventView.itemHeading = SDK.getMinistry().name
+        holder.eventView.itemContent = events.get(position).name
+        holder.eventView.itemFooter = events.get(position).date
+        if (events.get(position).isPast) {
+            holder.eventView.isHasType = true
+            holder.eventView.badge = "past"
+            holder.eventView.itemType = ListItemView3.ItemType.RED
+        }
     }
 }
--- events/src/main/java/net/faithgen/events/models/Event.kt
+++ events/src/main/java/net/faithgen/events/models/Event.kt
@@ -10,7 +10,7 @@ class Event {
     var date : String? = null
     var published: Boolean? = null
     @SerializedName("is_past")
-    var isPast: Boolean? = null
+    var isPast: Boolean = false
     var start: Date? = null
     var end: Date? = null
     var location: Location? = null
